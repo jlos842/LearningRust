b1f812f72d9996dac69548ac7e34cd628e0f3a98
Joe Los <jlos484@gmail.com>
2019-06-21 21:44:35 -0600

    Work on Chapter 1 and add notes

---
+++ Chapter1/hello_cargo/target/debug/deps/hello_cargo-2773c05304224b24.d
@@ -0,0 +1,5 @@
+/home/jolo/Documents/LearningRust/Chapter1/hello_cargo/target/debug/deps/hello_cargo-2773c05304224b24: src/main.rs
+
+/home/jolo/Documents/LearningRust/Chapter1/hello_cargo/target/debug/deps/hello_cargo-2773c05304224b24.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/jolo/Documents/LearningRust/Chapter1/hello_cargo/target/debug/hello_cargo: /home/jolo/Documents/LearningRust/Chapter1/hello_cargo/src/main.rs
