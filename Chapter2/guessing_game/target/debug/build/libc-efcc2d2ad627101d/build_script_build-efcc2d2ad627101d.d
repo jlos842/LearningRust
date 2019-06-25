1379579232522aac11fcc642fd9e53d966b5d678
Joe Los <jlos484@gmail.com>
2019-06-24 22:43:55 -0600

    Chapter 2 halfway point

---
+++ Chapter2/guessing_game/target/debug/build/libc-efcc2d2ad627101d/build_script_build-efcc2d2ad627101d.d
@@ -0,0 +1,5 @@
+/home/jolo/Documents/LearningRust/Chapter2/guessing_game/target/debug/build/libc-efcc2d2ad627101d/build_script_build-efcc2d2ad627101d: /home/jolo/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.58/build.rs
+
+/home/jolo/Documents/LearningRust/Chapter2/guessing_game/target/debug/build/libc-efcc2d2ad627101d/build_script_build-efcc2d2ad627101d.d: /home/jolo/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.58/build.rs
+
+/home/jolo/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.58/build.rs:
