f7a183d145814d5632ff68055f7f6a4719a8fd4b
Joe Los <jlos484@gmail.com>
2019-07-05 23:03:04 -0600

    Work on chapter 3

---
+++ Chapter3/variables/target/debug/deps/variables-465d6e9d4a043429.d
@@ -0,0 +1,5 @@
+/home/jolo/Documents/LearningRust/Chapter3/variables/target/debug/deps/variables-465d6e9d4a043429: src/main.rs
+
+/home/jolo/Documents/LearningRust/Chapter3/variables/target/debug/deps/variables-465d6e9d4a043429.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter3/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/jolo/Documents/LearningRust/Chapter3/variables/target/debug/variables: /home/jolo/Documents/LearningRust/Chapter3/variables/src/main.rs
